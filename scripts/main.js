fcb8bd1be9179125662f74fd7fffb2efa0242602
Paul An <hakuliu@gmail.com>
2015-11-03 14:37:22 -0800

    Added a way to respond to key strokes. also display the time in ms since last press. I added jquery.min.js in so that I can access the website without being online for now.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var textX = 50;
 var textY = 50;
 var lasttime = 0;
+var dt = 0;
 function update() {
     textX += 1;
     textY += 1;
@@ -11,9 +12,27 @@ function draw() {
     canvas.fillStyle = "#000"; // Set color to black
     canvas.fillText("Sup Bro!", 50, 50);
 
+
+    canvas.fillText(""+dt, 50, 75);
+}
+
+$(function() {
+  window.keydown = {};
+
+  function keyName(event) {
+    return jQuery.hotkeys.specialKeys[event.which] ||
+      String.fromCharCode(event.which).toLowerCase();
+  }
+
+  $(document).bind("keydown", function(event) {
+    keydown[keyName(event)] = true;
     var d = new Date();
     var t = d.getTime();
-    var dt = t - lasttime;
-    canvas.fillText(""+dt, 50, 75);
+    dt = t - lasttime;
     lasttime = t;
-}
+  });
+
+  $(document).bind("keyup", function(event) {
+    keydown[keyName(event)] = false;
+  });
+});
