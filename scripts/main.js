0b2e429ba8581ba902fbdcdef0817f114e74b491
Paul An <hakuliu@gmail.com>
2015-11-03 12:13:49 -0800

    Added a way to check time past per frame. this will give us a good way to check accuracy of keystrokes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 var textX = 50;
 var textY = 50;
-
+var lasttime = 0;
 function update() {
     textX += 1;
     textY += 1;
 }
 function draw() {
+    canvas.fillStyle = "#ff0";
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     canvas.fillStyle = "#000"; // Set color to black
-    canvas.fillText("Sup Bro!", textX, textY);
+    canvas.fillText("Sup Bro!", 50, 50);
+
+    var d = new Date();
+    var t = d.getTime();
+    var dt = t - lasttime;
+    canvas.fillText(""+dt, 50, 75);
+    lasttime = t;
 }
