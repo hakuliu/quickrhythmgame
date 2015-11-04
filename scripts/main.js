a9b64551b1378985b9d9a71686beb66f0fe83407
Paul An <hakuliu@gmail.com>
2015-11-03 21:39:30 -0800

    Scoring stuff.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var lasttime = 0;
 var dt = 0;
 var tempo_ms = Math.floor((Math.random() * 292) + 375)//DELAY IN MS between sounds.
 var nextExpected = 0;
-
+var totalscore = 0;
 
 function update() {
     var d = new Date();
@@ -19,6 +19,47 @@ function draw() {
     canvas.fillStyle = "#000"; // Set color to black
     canvas.fillText(tempo_ms, 50, 50);
     canvas.fillText(dt, 50, 75);
+    canvas.fillText(startervar, 50, 100);
+    canvas.fillText("Score: "+totalscore, 50, 125);
+}
+
+
+
+var startervar = 0;
+var currenttest = 0;
+function respondKey() {
+    var numstarter = 10;
+    var starterthresh = 60;//ms
+    t = getTimeMS();
+    //this is actually WRONG
+    //TODO: plz update to use expected time, not lasttime.
+    dt = t - lasttime;
+    lasttime = t;
+    difftest = Math.abs(dt - tempo_ms);
+    if(startervar < numstarter) {
+
+        if(difftest < starterthresh) {
+            startervar++;
+        } else {
+            startervar = 0;
+        }
+    } else {
+        var numtests = 20;
+        currenttest++;
+        if(currenttest <= numtests) {
+            ratio = difftest / parseFloat(tempo_ms);
+            score = 1 - ratio;
+            score = score * (1000.0/numtests);
+            totalscore = totalscore + score;
+            console.log("test " + currenttest + " score " + (1 - ratio) + ", " + score);
+        }
+    }
+}
+
+function getTimeMS() {
+    var d = new Date();
+    var t = d.getTime();
+    return t;
 }
 
 $(function() {
@@ -34,10 +75,7 @@ $(function() {
 
   $(document).bind("keydown", function(event) {
     keydown[keyName(event)] = true;
-    var d = new Date();
-    var t = d.getTime();
-    dt = t - lasttime;
-    lasttime = t;
+    respondKey();
   });
 
   $(document).bind("keyup", function(event) {
