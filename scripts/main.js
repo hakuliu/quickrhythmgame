d24b93b41c295e6af0bd1703ce1e0da34784f27c
Paul An <hakuliu@gmail.com>
2015-11-03 22:40:36 -0800

    trying to make the scoring more accurate. failing at it.

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,76 @@
 
-
-var lasttime = 0;
-var dt = 0;
 var tempo_ms = Math.floor((Math.random() * 292) + 375)//DELAY IN MS between sounds.
 var nextExpected = 0;
 var totalscore = 0;
+var lastSoundTime;//used in case you've really messed up.
 
 function update() {
-    var d = new Date();
-    var t = d.getTime();
-    if(t >= nextExpected) {
-        nextExpected = nextExpected + tempo_ms;
+    lastSoundTime = getTimeMS();
+    if(isSeeding() || testDone()) {
         Sound.play("kickmeaty");
     }
 }
 function draw() {
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     canvas.fillStyle = "#000"; // Set color to black
-    canvas.fillText(tempo_ms, 50, 50);
-    canvas.fillText(dt, 50, 75);
-    canvas.fillText(startervar, 50, 100);
+    canvas.fillText("Tempo is: " + tempo_ms, 50, 50);
+    canvas.fillText("Sound Ending in..." + (numstarter - startervar), 50, 100);
     canvas.fillText("Score: "+totalscore, 50, 125);
 }
 
 
-
-var startervar = 0;
 var currenttest = 0;
 function respondKey() {
-    var numstarter = 10;
-    var starterthresh = 60;//ms
     t = getTimeMS();
-    //this is actually WRONG
-    //TODO: plz update to use expected time, not lasttime.
-    dt = t - lasttime;
-    lasttime = t;
-    difftest = Math.abs(dt - tempo_ms);
-    if(startervar < numstarter) {
-
-        if(difftest < starterthresh) {
+    difftest = Math.abs(t - nextExpected);
+    console.log("difftest: " + difftest);
+    if(isSeeding()) {
+        doSeedingBehavior(difftest);
+    } else {
+        doTest(difftest);
+    }
+    setNextExpected(difftest);
+}
+function doSeedingBehavior(difftest) {
+var starterthresh = 60;//ms
+    if(difftest < starterthresh) {
             startervar++;
         } else {
             startervar = 0;
         }
-    } else {
-        var numtests = 20;
-        currenttest++;
-        if(currenttest <= numtests) {
-            ratio = difftest / parseFloat(tempo_ms);
-            score = 1 - ratio;
-            score = score * (1000.0/numtests);
-            totalscore = totalscore + score;
-            console.log("test " + currenttest + " score " + (1 - ratio) + ", " + score);
-        }
+}
+function doTest(difftest) {
+    currenttest++;
+    if(currenttest <= numtests && difftest <= tempo_ms) {
+        ratio = parseFloat(difftest) / parseFloat(tempo_ms);
+        score = 1 - ratio;
+        score = score * (1000.0/numtests);
+        totalscore = totalscore + score;
+        console.log("test " + currenttest + " score " + difftest + ", " + score);
     }
 }
-
+function setNextExpected(diff) {
+    if(diff > tempo_ms) {
+        //you were REALLY off, so we go by the last sound time.
+        nextExpected = tempo_ms + lastSoundTime;
+    } else {
+        //you weren't that off, so we increment.
+        nextExpected = nextExpected + tempo_ms;
+    }
+}
 function getTimeMS() {
     var d = new Date();
     var t = d.getTime();
     return t;
+}
+var startervar = 0;
+var numstarter = 10;
+function isSeeding() {
+    return startervar < numstarter;
+}
+var numtests = 20;
+function testDone() {
+    return currenttest >= numtests;
 }
 
 $(function() {
